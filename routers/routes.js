5683c34030e3cd43640ef32ab3b75c4fda5a54b2
bugamehmet <bugamehmetcan@gmail.com>
2023-08-25 15:57:09 +0300

    routes-aciklama

--- routers/routes.js
+++ routers/routes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const connection = require('../db');
-const checkUserRole = require('../middlewares/role');
-const { yilx, gunx, ayx } = require('../utils/date');
-const { inserthakedisKapagi, inserthakedisRaporu, insertYapilanisler } = require('../utils/insert');
+const express = require('express'); // Express.js framework'ünü içe aktarır
+const connection = require('../db'); // Veritabanı bağlantısını içe aktarır
+const checkUserRole = require('../middlewares/role'); // Kullanıcı rolünü kontrol eden middleware'i içe aktarır
+const { yilx, gunx, ayx } = require('../utils/date'); // Tarih işlemleri için yardımcı modülü içe aktarır
+const { inserthakedisKapagi, inserthakedisRaporu, insertYapilanisler } = require('../utils/insert'); // Veritabanına veri eklemek için yardımcı modülleri içe aktarır
 const {
 	downloadinfoPDF,
 	birlesmisPDF,
@@ -10,21 +10,24 @@ const {
 	hakedisraporuPDF,
 	yapilanislerPDF,
 	userbirlesmisPDF,
-} = require('../utils/generatePdf');
-const sendSMSMiddleware = require('../middlewares/sendsms')
-//const deleteHakedis = require('../utils/deleteHakedis');
-const router = express.Router();
+} = require('../utils/generatePdf'); // PDF oluşturmak için yardımcı modülleri içe aktarır
+const sendSMSMiddleware = require('../middlewares/sendsms'); // SMS göndermek için middleware'i içe aktarır
 
+const router = express.Router(); // Express Router'ı oluşturur
 
+// Ana sayfa için GET isteği işleyicisi
 router.get('/', (req, res) => {
 
 	res.render('login', { message: req.flash('message') });
 	//res.sendFile(path.join(__dirname, '../views/html/login.html'));
 });
+// Giriş işlemi için POST isteği işleyicisi
 router.post('/', (req, res) => {
+	// Kullanıcı adı ve şifreyi istekten al
 	let username = req.body.username;
 	let password = req.body.password;
 
+	// Veritabanında kullanıcıyı sorgula
 	let query = 'select * from userTable where username = ? and password = ?';
 	let params = [username, password];
 	connection.query(query, params, (err, results) => {
@@ -34,12 +37,14 @@ router.post('/', (req, res) => {
 			res.redirect('/');
 			return;
 		} else if (results.length > 0) {
+			// Kullanıcı bulunduysa oturum verilerini sakla
 			const isim = results[0].isim;
 			req.session.isim = isim;
 			const userId = results[0].userId;
 			req.session.userId = userId;
 			const role = results[0].role;
 			req.session.role = role;
+			// Kullanıcı rolüne göre yönlendirme yap
 			if (role == 'admin') {
 				req.flash('message', [`${results[0].isim}`, 'Hoşgeldiniz']);
 				res.redirect(`/ihale-bilgileri/${userId}`);
@@ -56,6 +61,7 @@ router.post('/', (req, res) => {
 		res.end();
 	});
 });
+// Oturumu sonlandırma için GET isteği işleyicisi
 router.get('/logout', (req, res) => {
 	req.session.destroy((err) => {
 		if (err) {
@@ -64,16 +70,19 @@ router.get('/logout', (req, res) => {
 		res.redirect('/');
 	});
 });
+// PDF'leri göstermek için GET isteği işleyicileri
 router.get('/showPDF/:no/:s_id', (req, res) => {
 	const no = req.params.no;
 	const s_id = req.params.s_id;
 	birlesmisPDF(res, no, s_id);
 });
+// PDF'leri indirmek için GET isteği işleyicileri
 router.get('/downloadPDF/:no/:s_id', (req, res) => {
 	const no = req.params.no;
 	const s_id = req.params.s_id;
 	downloadinfoPDF(res, no, s_id);
 });
+// Admin sayfasını görüntülemek için GET isteği işleyicisi
 router.get('/info', checkUserRole('admin'), (req, res) => {
 	const userId = req.session.userId;
 	const query = 'SELECT * FROM haz_hakedis_2 WHERE kullanici_id=? order by isin_adi desc';
@@ -93,13 +102,16 @@ router.get('/deletehakedis/:kullanici_id/:s_id/:no', (req, res) => {
 });
 */
 
+// '/ihale-bilgileri/:userId' URL'sine gelen GET isteği işleyicisi
 router.get('/ihale-bilgileri/:userId', (req, res) => {
+	// 'ihale-bilgileri' sayfasını render eder ve bir mesaj gönderir
 	res.render('ihale-bilgileri', { message: req.flash('message') });
-	//res.sendFile(path.join(__dirname, '../views/html/ihale-bilgileri.html'));
 });
+// '/ihale-bilgileri' URL'sine gelen POST isteği işleyicisi
 router.post('/ihale-bilgileri', async (req, res) => {
 	const userId = req.session.userId;
 
+	// Formdan gelen verileri alır
 	let uygulama_yili = yilx();
 	let tarih = `${gunx()}.${ayx()}.${yilx()}`;
 	let is_adi = req.body.is_adi;
@@ -114,6 +126,7 @@ router.post('/ihale-bilgileri', async (req, res) => {
 	let is_bitim_tarihi = req.body.is_bitim_tarihi;
 	let sirket_id = req.body.sirket_id;
 
+	// Şirket ID'si için bir sorgu yapar ve eğer kullanılıyorsa hata mesajı gösterir
 	let query = 'select s_id from hakedis_raporu where s_id=?';
 	let params = [sirket_id];
 	connection.query(query, params, async (err, results) => {
@@ -125,6 +138,7 @@ router.post('/ihale-bilgileri', async (req, res) => {
 			res.redirect(`/ihale-bilgileri/${userId}`);
 		} else {
 			try {
+				// Verileri veritabanına ekler
 				await inserthakedisKapagi(
 					userId,
 					sirket_id,
@@ -156,11 +170,14 @@ router.post('/ihale-bilgileri', async (req, res) => {
 		}
 	});
 });
+// '/register' URL'sine gelen GET isteği işleyicisi
 router.get('/register', (req, res) => {
-	//res.sendFile(path.join(__dirname, '../views/html/register.html'));
+	// 'register' sayfasını render eder ve bir mesaj gönderir
 	res.render('register', { message: req.flash('message') });
 });
+// '/register' URL'sine gelen POST isteği işleyicisi
 router.post('/register', (req, res) => {
+		// Formdan gelen verileri alır
 	let username = req.body.username;
 	let password = req.body.password;
 	let kurum_id = req.body.kurum_id;
@@ -170,6 +187,7 @@ router.post('/register', (req, res) => {
 	let telefon = req.body.telefon;
 	let adres = req.body.adres;
 
+	// Kullanıcı adının kullanılıp kullanılmadığını kontrol eder
 	let username_kullaniliyor_mu = 'select username from userTable where username=?';
 	let username_params = [username];
 	connection.query(username_kullaniliyor_mu, username_params, (err, results) => {
@@ -180,6 +198,7 @@ router.post('/register', (req, res) => {
 			req.flash('message', ['HATA !', 'Bu Kullanıcı Adı Kayıtlı']);
 			res.redirect(`/register`);
 		} else {
+				// E-postanın kullanılıp kullanılmadığını kontrol eder
 			let eposta_kullaniliyor_mu = 'select eposta from userTable where eposta=?';
 			let eposta_params = [eposta];
 			connection.query(eposta_kullaniliyor_mu, eposta_params, (err, results) => {
@@ -190,6 +209,7 @@ router.post('/register', (req, res) => {
 					req.flash('message', ['HATA !', 'Eposta Zaten Kayıtlı']);
 					res.redirect(`/register`);
 				} else {
+					// Telefon numarasının kullanılıp kullanılmadığını kontrol eder
 					let telefon_kullaniliyor_mu = 'select telefon from userTable where telefon=?';
 					let telefon_params = [telefon];
 					connection.query(telefon_kullaniliyor_mu, telefon_params, (err, results) => {
@@ -200,6 +220,7 @@ router.post('/register', (req, res) => {
 							req.flash('message', ['HATA !', 'Telefon Numarası Kayıtlı']);
 							res.redirect(`/register`);
 						} else {
+							// Kullanıcıyı veritabanına ekler
 							let query =
 								'INSERT INTO userTable (username, password, userId, isim, soyisim, eposta, telefon, adres) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
 							let params = [username, password, kurum_id, isim, soyisim, eposta, telefon, adres];
@@ -209,6 +230,7 @@ router.post('/register', (req, res) => {
 									res.redirect(`/register`);
 								} else {
 									req.flash('message', ['BAŞARILI !', 'Kullanıcı Başarıyla Kaydedildi']);
+									// SMS gönderme işlemi burada gerçekleşiyor
 									sendSMSMiddleware(username, password);
 									res.redirect(`/register`);
 								}
@@ -221,12 +243,14 @@ router.post('/register', (req, res) => {
 		}
 	});
 });
+// '/hakedis-kapagi/:userId' URL'sine gelen GET isteği işleyicisi
 router.get('/hakedis-kapagi/:userId', (req, res) => {
 	const useridInfo = req.params.userId;
 	const sirket_id = req.session.sirket_id;
 	hakediskapagiPDF(res, useridInfo, sirket_id);
 	req.session.sirket_id = null;
 });
+// '/hakedis-kapagi' URL'sine gelen POST isteği işleyicisi
 router.post('/hakedis-kapagi', (req, res) => {
 	const userId = req.session.userId;
 	const sirket_id = req.body.sirket_id;
@@ -243,6 +267,7 @@ router.post('/hakedis-kapagi', (req, res) => {
 		res.end();
 	});
 });
+// '/hakedis-kapagi' URL'sine gelen POST isteği işleyicisi
 router.get('/hakedis-raporu/:userId', (req, res) => {
 	const useridInfo = req.params.userId;
 	const gecikme = req.session.gecikme;
@@ -251,6 +276,7 @@ router.get('/hakedis-raporu/:userId', (req, res) => {
 	const hakedis_tutari = req.session.hakedis_tutari;
 	const kesinti = req.session.kesinti;
 	const sirket_id = req.session.sirket_id;
+	// Hakedis raporu PDF'ini oluşturur ve gösterir
 	hakedisraporuPDF(
 		res,
 		useridInfo,
@@ -261,8 +287,10 @@ router.get('/hakedis-raporu/:userId', (req, res) => {
 		kesinti,
 		sirket_id
 	);
+	// sirket_id yi sıfırlıyoruz çünkü admin sayfasında id girince kayırlı kalıyor başka id girince önceki id ile oluşturuyor tekrar
 	req.session.sirket_id = null;
 });
+// '/hakedis-raporu' URL'sine gelen POST isteği işleyicisi
 router.post('/hakedis-raporu', (req, res) => {
 	const userId = req.session.userId;
 	const gecikme = req.body.gecikme;
@@ -290,6 +318,7 @@ router.post('/hakedis-raporu', (req, res) => {
 		res.end();
 	});
 });
+// '/yapilan-isler/:userId' URL'sine gelen GET isteği işleyicisi
 router.get('/yapilan-isler/:userId', (req, res) => {
 	const useridInfo = req.params.userId;
 	const hakedis_tutari_2 = req.session.hakedis_tutari_2;
@@ -297,6 +326,7 @@ router.get('/yapilan-isler/:userId', (req, res) => {
 	yapilanislerPDF(res, useridInfo, hakedis_tutari_2, sirket_id);
 	req.session.sirket_id = null;
 });
+// '/yapilan-isler' URL'sine gelen POST isteği işleyicisi
 router.post('/yapilan-isler', (req, res) => {
 	const userId = req.session.userId;
 	const hakedis_tutari_2 = req.body.hakedis_tutari_2;
@@ -316,6 +346,8 @@ router.post('/yapilan-isler', (req, res) => {
 		res.end();
 	});
 });
+// '/userinfo' URL'sine gelen GET isteği işleyicisi
+
 router.get('/userinfo', checkUserRole('user'), (req, res) => {
 	const userId = req.session.userId;
 	const query = 'SELECT * FROM haz_hakedis_2 WHERE kullanici_id=? order by isin_adi desc';
@@ -324,6 +356,8 @@ router.get('/userinfo', checkUserRole('user'), (req, res) => {
 		res.render('userinfo', { userId, data });
 	});
 });
+// '/userHome/:userId' URL'sine gelen GET isteği işleyicisi
+
 router.get('/userHome/:userId', (req, res) => {
 	const userId = req.session.userId;
 	const query = 'SELECT * FROM haz_hakedis_2 WHERE kullanici_id=?';
@@ -332,6 +366,8 @@ router.get('/userHome/:userId', (req, res) => {
 		res.render('userHome', { data });
 	});
 });
+// '/pdf-olustur/:userId' URL'sine gelen GET isteği işleyicisi
+
 router.get('/pdf-olustur/:userId', (req, res) => {
 	const useridInfo = req.params.userId;
 	const gecikme = req.session.gecikme;
@@ -353,6 +389,8 @@ router.get('/pdf-olustur/:userId', (req, res) => {
 	);
 	req.session.sirket_id = null;
 });
+// '/pdf-olustur' URL'sine gelen POST isteği işleyicisi
+
 router.post('/pdf-olustur', (req, res) => {
 	const userId = req.session.userId;
 	const gecikme = req.body.gecikme;
