568e266776cb7469b30455d707cc94afed271094
bugamehmet <bugamehmetcan@gmail.com>
2023-08-25 15:32:52 +0300

    app.js-açıklama-satırları

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const session = require('express-session');
-const flash = require('connect-flash');
-const loggerMiddleware = require('./middlewares/logger'); //
+const express = require('express'); // Express.js framework'ünü içe aktarır.
+const session = require('express-session'); // Kullanıcı oturumlarını yönetmek için modül.
+const flash = require('connect-flash'); // flash mesajlar için 'connect-flash' modülü.
+const loggerMiddleware = require('./middlewares/logger'); // winston-loggerı kullandık.
 
-const app = express();
-app.set('view engine', 'ejs');
+const app = express(); //  Express uygulamasını başlatır.
+app.set('view engine', 'ejs'); // EJS, HTML içinde JavaScript kullanmak için.
 
-app.use('/assets', express.static('assets'));
-app.use(express.static('views'));
-app.use(express.urlencoded({ extended: true }));
-app.use(
+app.use('/assets', express.static('assets')); // 'assets' klasöründeki statik dosyaları sunmak için Express'in express.static middleware'ini kullandık.
+
+app.use(express.static('views')); // 'views' klasöründeki statik dosyaları sunmak için
+app.use(express.urlencoded({ extended: true })); // URL kodlu veriyi işlemek için Express'in yerleşik bir middleware'ini kullanır. Bu, POST isteklerinden gelen form verilerini işlemenize yardımcı olur. 
+app.use( // Oturum yönetimi için gerekli ayarları yapar.
 	session({
 		secret: 'GIZLI', // Bu gizli anahtarı değiştirin
 		resave: false,
 		saveUninitialized: true,
 	})
 );
-app.use(loggerMiddleware);
-app.use(flash({ unsafe: true }));
+app.use(loggerMiddleware); // GET-POST İSTEKLERİNİ LOGLUYORUZ .
+app.use(flash({ unsafe: true })); // Flash mesajları için 'connect-flash' middleware'ini kullanır.
 
 
-const router = require('./routers/routes');
-app.use('/', router);
+const router = require('./routers/routes'); // Rotaları yönetmek için bir router dosyasını içe aktarır.
 
-// MySQL bağlantısını kapat
-// connection.end();
+app.use('/', router); //  Rotaları uygulamaya bağlar.
+
+// Sunucuyu belirtilen portta dinlemeye başla
 app.listen(5001, () => {
 	console.log('Server http://localhost:5001 adresinde başladı');
 });
